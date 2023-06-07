61ebdfcb81c0426b70e8c78a66a1f4a1bd8702e6
slchahmed <108696292+slchahmed@users.noreply.github.com>
2023-06-07 19:39:18 +0000

    start

--- src/app/full-calendar/full-calendar.component.ts
+++ src/app/full-calendar/full-calendar.component.ts
@@ -1,29 +1,68 @@
 import { Component } from '@angular/core';
-import { CalendarOptions } from '@fullcalendar/core';
+import { CalendarOptions, Calendar } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
-import interactionPlugin from '@fullcalendar/interaction'
 import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
 @Component({
   selector: 'app-full-calendar',
   templateUrl: './full-calendar.component.html',
   styleUrls: ['./full-calendar.component.css']
 })
 export class FullCalendarComponent {
-  calendarOptions: CalendarOptions = {
-    plugins: [timeGridPlugin,dayGridPlugin],
-    initialView: 'timeGridWeek',
-    height:500,
-    themeSystem:'bootstrap5',
-    aspectRatio:16,
-    headerToolbar: {
-      center: 'prev,title,next',
-      right: 'timeGridWeek,timeGridDay,dayGridMonth' // user can switch between the two
-    },
+  calendarOptions!: CalendarOptions;
+  calendar!: Calendar;
+
+  ngOnInit() {
+    this.calendarOptions = {
+      editable: true, // Enable dragging and resizing of events
+      events: 'https://fullcalendar.io/api/demo-feeds/events.json?overload-day',
+      eventColor: '#0FAF9B',
+      plugins: [timeGridPlugin, dayGridPlugin,interactionPlugin],
+      initialView: 'timeGridWeek',
+      height: 700,
+      aspectRatio: 16,
+      headerToolbar: {
+        left: 'Planning',
+        center: 'prev,title,next',
+        right: 'dayGridMonth,timeGridWeek,addEventButton'
+      },
+      weekNumbers: false,
+      dayMaxEvents: true,
+      titleFormat: { month: 'long', year: 'numeric' },
+      selectable: true,
+      selectMirror: true,
+      allDaySlot: false,
+      dayHeaderFormat: { weekday: 'short', day: 'numeric' },
+      viewDidMount: (args) => {
+        const axisFrameDiv = document.querySelector('.fc-timegrid-axis-frame') as HTMLElement;
+        axisFrameDiv.innerHTML = '<div class="custom-content">Employee</div>';
+        const headerToolbar = document.querySelector('.fc-toolbar-chunk') as HTMLElement;
+        headerToolbar.insertAdjacentHTML('afterbegin', '<div class="custom-content2">Planning</div>');
+
+        this.calendar = args.view.calendar as Calendar;
+      },
+      customButtons: {
+        addEventButton: {
+          text: 'add event...',
+          click: () => {
+            var dateStr = prompt('Enter a date in YYYY-MM-DD format');
+            var date = new Date(dateStr + 'T00:00:00');
+
+            if (!isNaN(date.valueOf())) {
+              this.calendar.addEvent({
+                title: 'dynamic event',
+                start: date,
+                allDay: true
+              });
+              alert('Great. Now, update your database...');
+            } else {
+              alert('Invalid date.');
+            }
+          }
+        }
+      }
+      
+    };
     
-    weekNumbers: true,
-    dayMaxEvents: true, // allow "more" link when too many events
-    
-    selectable: true,
-    selectMirror:true,
-  };
+  }
 }
